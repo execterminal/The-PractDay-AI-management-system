68bf23076dea86853fa98ef4001774378ac7daac
execterminal <boba3ma@outlook.com>
2026-05-12 13:58:44 +0500

    MainMenu array

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -3,135 +3,81 @@ import { FaLocationCrosshairs } from "react-icons/fa6";
 import { BsJournal } from "react-icons/bs";
 import { FaBell } from "react-icons/fa";
 import { GiReceiveMoney } from "react-icons/gi";
+
 import { Button } from "./ui/Button";
 import { useTranslation } from "react-i18next";
 
+const menuItems = [
+  {
+    icon: RiSchoolFill,
+    label: "home.place",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+  {
+    icon: BsJournal,
+    label: "home.journal",
+  },
+  {
+    icon: FaBell,
+    label: "home.requests",
+  },
+  {
+    icon: GiReceiveMoney,
+    label: "home.practice",
+  },
+  {
+    icon: GiReceiveMoney,
+    label: "home.practice",
+  },
+  {
+    icon: GiReceiveMoney,
+    label: "home.practice",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+  {
+    icon: FaLocationCrosshairs,
+    label: "home.judgment",
+  },
+];
+
 export function MainMenu() {
   const { t } = useTranslation();
+
   return (
-    <div className="flex items-center justify-center min-h-screen border border-green-700">
-      <div className="w-7/8 h-6/8 flex items-center justify-center border-2 border-border p-10 rounded-2xl bg-secondary">
-        <div className="wrap-break-words text-center items-center justify-center">
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <RiSchoolFill className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.place")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <BsJournal className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.journal")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaBell className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.requests")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <GiReceiveMoney className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.practice")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <GiReceiveMoney className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.practice")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <GiReceiveMoney className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.practice")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
-          <Button
-            className="w-5/6 h-5/6 flex items-center justity-center flex-col"
-            size="element"
-            margin="element"
-          >
-            <FaLocationCrosshairs className="w-25 h-25" />
-            <div className="pt-2 whitespace-normal wrap-break-words">
-              {t("home.judgment")}
-            </div>
-          </Button>
+    <div className="fc min-h-screen border border-green-700">
+      <div className="w-7/8 h-6/8 rounded-2xl border-2 border-border bg-secondary p-10">
+        <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+          {menuItems.map(({ icon: Icon, label }) => (
+            <Button
+              className="fc h-48 w-full justify-center gap-3"
+              size="element"
+              margin="element"
+            >
+              <Icon className="h-20 w-20" />
+
+              <div className="max-w-[120px] text-center wrap-break-words whitespace-normal">
+                {t(label)}
+              </div>
+            </Button>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -20,7 +20,7 @@ const Signup: React.FC = () => {
   return (
     <div>
       <Header></Header>
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="fc min-h-screen">
         <div className="border-2 border-border p-10 rounded-2xl bg-secondary">
           <h1 className="text-3xl mb-8">{t("signup1.reg")}</h1>
 
